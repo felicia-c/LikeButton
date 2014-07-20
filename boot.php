3ea0b9c5747fd97afd7a629397e7cc9e260662e4
Alexandre Hermann <hermann.alexandre@ahwebdev.fr>
2014-07-20 04:14:26 +0200

    dev

--- boot.php
+++ boot.php
@@ -4,8 +4,8 @@
   Plugin Name: Facebook AWD Like Button
   Plugin URI: http://facebook-awd.ahwebdev.fr
   Description: Facebook AWD Like button adds facebook like button on your site
-  Version: 2.0-dev
-  Author: AHWEBDEV
+  Version: 2.0
+  Author: Alexandre Hermann (AHWEBDEV) <hermann.alexandre@ahwebdev.fr>
   Author URI: http://www.ahwebdev.fr
   License: Copywrite AHWEBDEV
   Text Domain: FacebookAWD
